3c35da7cf353f6516de0ac5c1b8de35f044eccf1
Pranavi-2002 <101917240+Pranavi-2002@users.noreply.github.com>
2024-02-27 11:54:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 notes = []
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def index():
-    note = request.args.get("note")
-    notes.append(note)
+    if request.method=="POST":
+        note = request.form.get("note")
+        if note:
+            notes.append(note)
     return render_template("home.html", notes=notes)
 
 
